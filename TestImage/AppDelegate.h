75331dc8113f7bdba38b55b17e21a31d7f34c3d4
ankitmpasteldreams <32053948+ankitmpasteldreams@users.noreply.github.com>
2017-10-31 12:26:56 +0530

    Initial Commit

---
+++ TestImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestImage
+//
+//  Created by Pastel Dreams on 31/10/17.
+//  Copyright © 2017 Pastel Dreams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestImage
+//
+//  Created by Pastel Dreams on 31/10/17.
+//  Copyright © 2017 Pastel Dreams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
